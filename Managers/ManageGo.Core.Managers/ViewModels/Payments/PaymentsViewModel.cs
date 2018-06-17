12e9e7e1b0786b7c0d4f41763414409cab97241b
Robert Hedgpeth <rob.hedgpeth@gmail.com>
2018-06-17 15:09:08 -0400

    Tuning Android drawer layout performance

--- Managers/ManageGo.Core.Managers/ViewModels/Payments/PaymentsViewModel.cs
+++ Managers/ManageGo.Core.Managers/ViewModels/Payments/PaymentsViewModel.cs
@@ -15,27 +15,34 @@ namespace ManageGo.Core.Managers.ViewModels
 
 		public override async Task InitAsync()
 		{
-			var items = new List<object>();
+			await Task.Delay(250).ConfigureAwait(false);
+                     
+			var taskItems = await Task.Run(() =>
+			{
+				var items = new List<object>();
 
-            for (int i = 0; i < 10; i++)
-            {
-                items.Add(new PaymentSectionHeaderViewModel 
-				              { 
-					                Amount = string.Format("${0:#.00}", Convert.ToDecimal(RandomNumberBetween(1,10000) / 100)), 
-					                PaymentDateTime = DateTime.Now.AddDays(-10).AddDays(i).ToShortDateString() + " 8:00 AM",
-					                Name = $"Person {i}",
-					                Address = $"1234 Main St. Apt. #{i}",
-					                Children = new List<object> 
-					                { 
-						                new PaymentDetailsViewModel 
-						                { 
-						                    Description = $"This is a description for payment {i}." 
-						                }
-					                }  
-				                });
-            }
+				for (int i = 0; i < 10; i++)
+				{
+					items.Add(new PaymentSectionHeaderViewModel
+					{
+						Amount = string.Format("${0:#.00}", Convert.ToDecimal(RandomNumberBetween(1, 10000) / 100)),
+						PaymentDateTime = DateTime.Now.AddDays(-10).AddDays(i).ToShortDateString() + " 8:00 AM",
+						Name = $"Person {i}",
+						Address = $"1234 Main St. Apt. #{i}",
+						Children = new List<object>
+									{
+										new PaymentDetailsViewModel
+										{
+											Description = $"This is a description for payment {i}."
+										}
+									}
+					});
+				}
 
-			Items = new ObservableCollection<object>(items);
+				return items;
+			});
+
+			Items = new ObservableCollection<object>(taskItems);
 		}      
 	}
 }
